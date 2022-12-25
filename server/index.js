7dd2591ee11ab51fed192a7e89d33c9ed57c549c
william fred <william.fred@canea.se>
2022-12-25 20:03:04 +0100

    sending messages no working

--- server/index.js
+++ server/index.js
@@ -11,10 +11,9 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on("connection" =>{
-    
-  })
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
