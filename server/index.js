6450b7f579d4170f8f71a3d36c9be0bbb0d4d38a
william fred <william.fred@canea.se>
2022-12-25 18:53:29 +0100

    socket io

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
+
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send("<h1> hello world </h1>");
+    res.sendFile('index.html', { root: '../client/' });
 });
 
-server.listen(3000, () =>{
-    console.log("listen on *.3000");
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on("connection" =>{
+    
+  })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
